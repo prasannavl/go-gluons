3c30d13ab2073df7a7ed8984414866c847f9217f
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-07-24 22:32:27 +0530

    refactor: robust deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,58 +1,56 @@
 #!/usr/bin/env bash
 ## Author: Prasanna V. Loganathar
 
-set -e
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
-cd ${dir}
-
 binary_name="nextfirst-core"
-dest_dir="${HOME}/workspace/bin/"
+deploy_dir="${HOME}/workspace/bin/"
 logs_dir="${HOME}/workspace/logs/"
 
-# Build
+main() {
+    set -e
+    local dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"    
+    cd ${dir}
+    build
+    deploy
+    cd ${deploy_dir}    
+    start
+}
 
-set +e
-echo "> build: start"
+build() {
+    echo "> build: start"
+    go get -v -u github.com/golang/dep/cmd/dep || true
+    dep ensure || true
+    go build -o "./bin/${binary_name}"
+    echo "> build: success"
+}
 
-go get -v -u github.com/golang/dep/cmd/dep
-dep ensure
-go build -o ./bin/nextfirst-core
+deploy() {
+    echo "> deploy: start"
+    mkdir -p "$deploy_dir"
+    wait_for_process_briefly "$binary_name"
+    mv -f "./bin/${binary_name}" "$deploy_dir"
+    echo "> deploy: success"
+}
 
-ret_val=$?
-if [ $ret_val -ne 0 ]; then
-    echo "> build: fail"    
-    exit $ret_val
-fi
-
-echo "> build: success"
-
-# Deploy
-
-set -e
-echo "> deploy: start"
-mkdir -p "$dest_dir"
-mkdir -p "$logs_dir"
+start() {
+    echo "> run: start"
+    mkdir -p "$logs_dir"
+    binary_path="${deploy_dir}/${binary_name}"
+    sudo setcap cap_net_bind_service=+ep "$binary_path"
+    nohup "$binary_path address=':80'" &>> "${logs_dir}/${binary_name}-run.log" &
+    echo "> run: success"
+}
 
 wait_for_process_briefly(){
-    local pid=$(pidof "$@")
+    local pid=$(pidof "$@" || "")
     if [ -z "$pid" ]; then return; fi;
+    local d=900
+    echo "> deploy: waiting for previous shutdown.. (max:${$((d/10))}s)"
     local i=0
     while kill -0 "$pid"; do
         sleep 0.1s
         i=$((i+1))
-        if [ $i -gt 900 ]; then kill -9 "$pid"; fi;
+        if [ $i -gt $d ]; then kill -9 "$pid" || true; fi;
     done
 }
 
-wait_for_process_briefly "$binary_name"
-mv -f "./bin/${binary_name}" "$dest_dir"
-echo "> deploy: success"
-
-# Run
-
-echo "> run: start"
-cd ${dest_dir}
-binary_path="${dest_dir}/${binary_name}"
-sudo setcap cap_net_bind_service=+ep "$binary_path"
-nohup "$binary_path" address=":80" &>> "${logs_dir}/${binary_name}-run.log" &
-echo "> run: success"
+main $@
