d555dcb2b1ef8383409951087eda2826b3b531b0
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-07-24 19:08:57 +0530

    fix: handle fresh deploy properly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,15 +8,23 @@ cd ${dir}
 binary_name="nextfirst-core"
 dest_path="~/workspace/bin/"
 
+set +e
 # Build
-go get -u github.com/golang/dep/cmd/dep
-dep ensure && go build -o ./bin/nextfirst-core
+go get -u github.com/golang/dep/cmd/dep && dep ensure
+go build -o ./bin/nextfirst-core
 
+ret_val=$?
+if [ $ret_val -ne 0 ]; then
+    exit $ret_val
+fi
+
+set -e
 # Deploy
 mkdir -p "$dest_path"
 
 wait_for_process_briefly(){
     local pid=$(pidof "$@")
+    if [ -z "$pid" ]; then return; fi;
     local i=0
     while kill -0 "$pid"; do
         sleep 0.1s
