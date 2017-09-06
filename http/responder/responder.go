8922c1d416d90250c08c104e69b1c219e8354eb3
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-06 11:48:20 +0530

    change: simplify renderer for now

--- http/responder/responder.go
+++ http/responder/responder.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"html/template"
-
 	"github.com/prasannavl/goerror/httperror"
 	"github.com/unrolled/render"
 )
@@ -47,12 +45,6 @@ func Send(w http.ResponseWriter, r *http.Request, value interface{}) error {
 }
 
 func SendWithStatus(w http.ResponseWriter, r *http.Request, status int, value interface{}) error {
-	// TODO: Do negotiation here
-	if tcon, ok := value.(TemplateExecutor); ok {
-		SetStatus(w, status)
-		// TODO: Set proper headers before-hand. Meanwhile, net/http sniffs it, for now.
-		return tcon.Execute(w)
-	}
 	return Renderer.JSON(w, status, value)
 }
 
@@ -66,59 +58,4 @@ func SendError(w http.ResponseWriter, r *http.Request, err error) error {
 func sendHttpError(w http.ResponseWriter, r *http.Request, err httperror.HttpError) error {
 	msg := err.Error()
 	return SendWithStatus(w, r, err.Code(), msg)
-}
-
-// Template stuff
-
-type TemplateExecutor interface {
-	Execute(w http.ResponseWriter) error
-}
-
-type TemplateFilesContent struct {
-	Data          interface{}
-	TemplateFiles []string
-}
-
-func (t *TemplateFilesContent) Execute(w http.ResponseWriter) error {
-	tmp, err := template.ParseFiles(t.TemplateFiles...)
-	if err != nil {
-		return err
-	}
-	return tmp.Execute(w, t.Data)
-}
-
-type TemplateStringContent struct {
-	Data           interface{}
-	TemplateString string
-}
-
-func (t *TemplateStringContent) Execute(w http.ResponseWriter) error {
-	tmp := template.New("tmpl")
-	tmp, err := tmp.Parse(t.TemplateString)
-	if err != nil {
-		return err
-	}
-	return tmp.Execute(w, t.Data)
-}
-
-type TemplateContent struct {
-	Data     interface{}
-	Template template.Template
-}
-
-func (t *TemplateContent) Execute(w http.ResponseWriter) error {
-	return t.Template.Execute(w, t.Data)
-}
-
-type TemplateGlobContent struct {
-	Data interface{}
-	Glob string
-}
-
-func (t *TemplateGlobContent) Execute(w http.ResponseWriter) error {
-	tmp, err := template.ParseGlob(t.Glob)
-	if err != nil {
-		return err
-	}
-	return tmp.Execute(w, t.Data)
 }
