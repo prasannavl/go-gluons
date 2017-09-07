3f08f0123b1e061bc5acfedb163f6e855ac12033
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-08 00:06:44 +0530

    refactor: handler utils

--- http/utils/handlerutils.go
+++ http/utils/handlerutils.go
@@ -8,14 +8,29 @@ import (
 	"github.com/prasannavl/mchain"
 )
 
-func RunOnPrefix(prefix string, h mchain.Handler, w http.ResponseWriter, r *http.Request) (done bool, err error) {
+func OnPrefix(prefix string, h mchain.Handler, w http.ResponseWriter, r *http.Request) (done bool, err error) {
+	if strings.HasPrefix(r.URL.Path, prefix) {
+		return true, h.ServeHTTP(w, r)
+	}
+	return false, nil
+}
+
+func OnPrefixFunc(prefix string, f mchain.HandlerFunc, w http.ResponseWriter, r *http.Request) (done bool, err error) {
+	return OnPrefix(prefix, mchain.HandlerFunc(f), w, r)
+}
+
+func OnStrippedPrefix(prefix string, h mchain.Handler, w http.ResponseWriter, r *http.Request) (done bool, err error) {
 	if strings.HasPrefix(r.URL.Path, prefix) {
 		return true, middleware.StripPrefix(prefix, h).ServeHTTP(w, r)
 	}
 	return false, nil
 }
 
-func RunOnPrefixAndRedirectToSlash(prefix string, h mchain.Handler, w http.ResponseWriter, r *http.Request) (done bool, err error) {
+func OnStrippedPrefixFunc(prefix string, h mchain.HandlerFunc, w http.ResponseWriter, r *http.Request) (done bool, err error) {
+	return OnStrippedPrefix(prefix, mchain.HandlerFunc(h), w, r)
+}
+
+func OnStrippedPrefixAndRedirectToSlash(prefix string, h mchain.Handler, w http.ResponseWriter, r *http.Request) (done bool, err error) {
 	if strings.HasPrefix(r.URL.Path, prefix) {
 		if r.URL.Path == prefix {
 			path := r.URL.Host + middleware.ConstructPathFromStripped(r) + "/"
@@ -32,9 +47,15 @@ func RunOnPrefixAndRedirectToSlash(prefix string, h mchain.Handler, w http.Respo
 	return false, nil
 }
 
+func OnStrippedPrefixAndRedirectToSlashFunc(prefix string, f mchain.HandlerFunc, w http.ResponseWriter, r *http.Request) (done bool, err error) {
+	return OnStrippedPrefixAndRedirectToSlash(prefix, mchain.HandlerFunc(f), w, r)
+}
+
+// Handlers
+
 func Mount(prefix string, h mchain.Handler) mchain.Handler {
 	f := func(w http.ResponseWriter, r *http.Request) error {
-		_, err := RunOnPrefix(prefix, h, w, r)
+		_, err := OnStrippedPrefix(prefix, h, w, r)
 		return err
 	}
 	return mchain.HandlerFunc(f)
@@ -43,16 +64,28 @@ func Mount(prefix string, h mchain.Handler) mchain.Handler {
 func MountAndRedirectToSlash(prefix string, h mchain.Handler) mchain.Handler {
 	prefix = strings.TrimSuffix(prefix, "/")
 	f := func(w http.ResponseWriter, r *http.Request) error {
-		_, err := RunOnPrefixAndRedirectToSlash(prefix, h, w, r)
+		_, err := OnStrippedPrefixAndRedirectToSlash(prefix, h, w, r)
 		return err
 	}
 	return mchain.HandlerFunc(f)
 }
 
-func MountFunc(prefix string, h mchain.HandlerFunc) mchain.Handler {
-	return Mount(prefix, mchain.HandlerFunc(h))
+func MountFunc(prefix string, f mchain.HandlerFunc) mchain.Handler {
+	return Mount(prefix, mchain.HandlerFunc(f))
 }
 
-func MountFuncAndRedirectToSlash(prefix string, h mchain.HandlerFunc) mchain.Handler {
-	return MountAndRedirectToSlash(prefix, mchain.HandlerFunc(h))
+func MountFuncAndRedirectToSlash(prefix string, f mchain.HandlerFunc) mchain.Handler {
+	return MountAndRedirectToSlash(prefix, mchain.HandlerFunc(f))
+}
+
+func HandlerOnPrefix(prefix string, h mchain.Handler) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		_, err := OnPrefix(prefix, h, w, r)
+		return err
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func HandlerFuncOnPrefix(prefix string, f mchain.HandlerFunc) mchain.Handler {
+	return HandlerOnPrefix(prefix, mchain.HandlerFunc(f))
 }
