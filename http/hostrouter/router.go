877cd9c9f14827f5e8227a9dcef5aff38b89ecac
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-10 01:17:42 +0530

    add: handlePattern

--- http/hostrouter/router.go
+++ http/hostrouter/router.go
@@ -2,15 +2,24 @@ package hostrouter
 
 import (
 	"net/http"
+
+	"github.com/gobwas/glob"
 )
 
 type HostRouter struct {
-	Items     interface{}
-	Threshold int
+	Items        interface{}
+	Threshold    int
+	PatternItems []RouterGlobItem
 }
 
-type HostRouterItem struct {
-	name    string
+type RouterItem struct {
+	host    string
+	handler http.Handler
+}
+
+type RouterGlobItem struct {
+	pattern string
+	matcher glob.Glob
 	handler http.Handler
 }
 
@@ -20,21 +29,35 @@ func New() *HostRouter {
 
 func (h *HostRouter) Build(notFoundHandler http.Handler) http.Handler {
 	if items, ok := h.Items.(map[string]http.Handler); ok {
-		h := func(w http.ResponseWriter, r *http.Request) {
-			if handler, ok := items[r.URL.Hostname()]; ok {
+		hh := func(w http.ResponseWriter, r *http.Request) {
+			hostname := r.Host
+			if handler, ok := items[hostname]; ok {
 				handler.ServeHTTP(w, r)
-			} else {
-				notFoundHandler.ServeHTTP(w, r)
+				return
 			}
+			for _, x := range h.PatternItems {
+				if x.matcher.Match(hostname) {
+					x.handler.ServeHTTP(w, r)
+					return
+				}
+			}
+			notFoundHandler.ServeHTTP(w, r)
 		}
-		return http.HandlerFunc(h)
+		return http.HandlerFunc(hh)
 	}
-	items := h.Items.([]HostRouterItem)
+	items := h.Items.([]RouterItem)
 	hx := func(w http.ResponseWriter, r *http.Request) {
 		for _, x := range items {
-			if x.name == r.URL.Hostname() {
+			hostname := r.Host
+			if x.host == hostname {
 				x.handler.ServeHTTP(w, r)
 				return
+			}
+			for _, x := range h.PatternItems {
+				if x.matcher.Match(hostname) {
+					x.handler.ServeHTTP(w, r)
+					return
+				}
 			}
 			notFoundHandler.ServeHTTP(w, r)
 		}
@@ -46,26 +69,26 @@ func (h *HostRouter) resolveContainer() {
 	switch item := h.Items.(type) {
 	case map[string]http.Handler:
 		if len(item) < h.Threshold+1 {
-			s := make([]HostRouterItem, 0, len(item)+1)
+			s := make([]RouterItem, 0, len(item)+1)
 			for k, v := range item {
-				s = append(s, HostRouterItem{name: k, handler: v})
+				s = append(s, RouterItem{host: k, handler: v})
 			}
 			h.Items = s
 		}
-	case []HostRouterItem:
+	case []RouterItem:
 		if len(item)+1 > h.Threshold {
 			m := make(map[string]http.Handler, len(item)+1)
 			for _, x := range item {
-				m[x.name] = x.handler
+				m[x.host] = x.handler
 			}
 			h.Items = m
 		}
 	default:
-		h.Items = []HostRouterItem{}
+		h.Items = []RouterItem{}
 	}
 }
 
-func (h *HostRouter) Set(host string, handler http.Handler) {
+func (h *HostRouter) HandleHost(host string, handler http.Handler) {
 	h.resolveContainer()
 	switch item := h.Items.(type) {
 	case map[string]http.Handler:
@@ -74,19 +97,19 @@ func (h *HostRouter) Set(host string, handler http.Handler) {
 		} else {
 			item[host] = handler
 		}
-	case []HostRouterItem:
+	case []RouterItem:
 		if handler == nil {
 			for i, x := range item {
-				if x.name == host {
+				if x.host == host {
 					// Remove item
 					h.Items = append(item[:i], item[i+1:]...)
 					break
 				}
 			}
 		} else {
-			route := HostRouterItem{host, handler}
+			route := RouterItem{host, handler}
 			for i, x := range item {
-				if x.name == host {
+				if x.host == host {
 					// Replace item
 					item[i] = route
 					return
@@ -95,4 +118,34 @@ func (h *HostRouter) Set(host string, handler http.Handler) {
 			h.Items = append(item, route)
 		}
 	}
+}
+
+func (h *HostRouter) HandlePattern(globPattern string, handler http.Handler) {
+	hasStar := false
+	for _, x := range globPattern {
+		if x == '*' {
+			hasStar = true
+			break
+		}
+	}
+
+	if !hasStar {
+		h.HandleHost(globPattern, handler)
+		return
+	}
+
+	items := h.PatternItems
+	// note: ok to copy into x during range
+	for i, x := range items {
+		if x.pattern == globPattern {
+			if handler == nil {
+				h.PatternItems = append(items[:i], items[i+1:]...)
+				return
+			}
+			items[i].handler = handler
+			return
+		}
+	}
+	h.PatternItems = append(items, RouterGlobItem{
+		pattern: globPattern, matcher: glob.MustCompile(globPattern), handler: handler})
 }
