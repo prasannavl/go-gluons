99e3a7504b47e229784e8d7be9b91a2eca9cbba5
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-11 09:29:30 +0530

    add: file handler

---
+++ http/utils/misc.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/prasannavl/mchain"
+)
+
+func CreateFileHandler(filePath string, status int) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		f, err := os.Open(filePath)
+		if err != nil {
+			return err
+		}
+		w.WriteHeader(status)
+		_, err = io.Copy(w, f)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return mchain.HandlerFunc(f)
+}
