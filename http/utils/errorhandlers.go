69a6a7196b00d8a9deb7ca6ff0a79aa396658781
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-13 16:12:48 +0530

    refactor: error handler helpers

--- http/utils/errorhandlers.go
+++ http/utils/errorhandlers.go
@@ -3,14 +3,54 @@ package utils
 import (
 	"net/http"
 
+	"github.com/prasannavl/goerror/httperror"
+	"github.com/prasannavl/mchain"
+
 	"github.com/prasannavl/go-gluons/log"
 )
 
-func InternalServerError(err error, w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
+var HttpCodeOrInternalServerError = CreateHttpErrorHandler(http.StatusInternalServerError)
+var LoggedHttpCodeOrInternalServerError = CreateHttpErrorHandler(http.StatusInternalServerError)
+
+var InternalServerError = CreateStatusErrorHandler(http.StatusInternalServerError)
+var LoggedInternalServerError = CreateLoggedStatusErrorHandler(http.StatusInternalServerError)
+
+var BadRequestError = CreateStatusErrorHandler(http.StatusBadRequest)
+var LoggedBadRequestError = CreateLoggedStatusErrorHandler(http.StatusBadRequest)
+
+func CreateLoggedStatusErrorHandler(status int) mchain.ErrorHandler {
+	return func(err error, w http.ResponseWriter, r *http.Request) {
+		log.Errorf("error-handler: %v", err)
+		w.WriteHeader(status)
+	}
 }
 
-func InternalServerErrorAndLog(err error, w http.ResponseWriter, r *http.Request) {
-	log.Errorf("error-handler: %v", err)
-	w.WriteHeader(http.StatusInternalServerError)
+func CreateStatusErrorHandler(status int) mchain.ErrorHandler {
+	return func(err error, w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+	}
+}
+
+func CreateLoggedHttpErrorHandler(fallbackStatus int) mchain.ErrorHandler {
+	return func(err error, w http.ResponseWriter, r *http.Request) {
+		if e, ok := err.(httperror.HttpError); ok {
+			if httperror.IsServerErrorCode(e.Code()) {
+				log.Errorf("error-handler: %v", e)
+			}
+			w.WriteHeader(e.Code())
+		} else {
+			log.Errorf("error-handler: %v", err)
+			w.WriteHeader(fallbackStatus)
+		}
+	}
+}
+
+func CreateHttpErrorHandler(fallbackStatus int) mchain.ErrorHandler {
+	return func(err error, w http.ResponseWriter, r *http.Request) {
+		if e, ok := err.(httperror.HttpError); ok {
+			w.WriteHeader(e.Code())
+		} else {
+			w.WriteHeader(fallbackStatus)
+		}
+	}
 }
--- http/utils/notfound.go
+++ http/utils/notfound.go
@@ -12,13 +12,13 @@ func HttpNotFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func NotFound(w http.ResponseWriter, r *http.Request) error {
-	return httperror.New(http.StatusNotFound, "", true)
-}
-
 // NotFoundHandler returns a simple request handler
 // that replies to each request with a ``404 page not found'' reply.
 func HttpNotFoundHandler() http.Handler { return http.HandlerFunc(HttpNotFound) }
+
+func NotFound(w http.ResponseWriter, r *http.Request) error {
+	return httperror.New(http.StatusNotFound, "", true)
+}
 
 func NotFoundHandler() mchain.Handler {
 	return mchain.HandlerFunc(NotFound)
