6fb7d1bc1b0ee61afbd20588a851e41e68c598f0
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-19 11:44:36 +0530

    refactor: delegate all inner methods to logCore

---
+++ log/global.go
@@ -0,0 +1,145 @@
+package log
+
+import "os"
+
+var (
+	g *Logger
+)
+
+func init() {
+	g = &Logger{
+		sink: &StreamSink{
+			Stream:    os.Stderr,
+			Formatter: DefaultTextFormatterForHuman,
+		},
+	}
+}
+
+func New(sink Sink) *Logger {
+	return &Logger{sink, AllLevelsFilter, nil, FlagTime}
+}
+
+func SetLogger(l *Logger) {
+	g = l
+}
+
+func GetLogger() *Logger {
+	return g
+}
+
+func GetSink(logger *Logger) Sink {
+	return logger.sink
+}
+
+func GetFilter(logger *Logger) func(Level) bool {
+	return logger.filter
+}
+
+func SetFilter(logger *Logger, filter func(Level) bool) {
+	if filter == nil {
+		filter = AllLevelsFilter
+	}
+	logger.filter = filter
+}
+
+func GetFields(logger *Logger) []Field {
+	return logger.fields
+}
+
+func GetFlags(logger *Logger) loggerFlags {
+	return logger.flags
+}
+
+func SetFlags(logger *Logger, flags loggerFlags) {
+	logger.flags = flags
+}
+
+// Log methods
+
+func Log(lvl Level, message string) {
+	logCore(g, lvl, message, nil, skipFramesNum)
+}
+
+func Error(message string) {
+	logCore(g, ErrorLevel, message, nil, skipFramesNum)
+}
+
+func Warn(message string) {
+	logCore(g, WarnLevel, message, nil, skipFramesNum)
+}
+
+func Info(message string) {
+	logCore(g, InfoLevel, message, nil, skipFramesNum)
+}
+
+func Debug(message string) {
+	logCore(g, DebugLevel, message, nil, skipFramesNum)
+}
+
+func Trace(message string) {
+	logCore(g, TraceLevel, message, nil, skipFramesNum)
+}
+
+// Logv methods
+
+func Logv(lvl Level, args ...interface{}) {
+	logCore(g, lvl, "", args, skipFramesNum)
+}
+
+func Errorv(args ...interface{}) {
+	logCore(g, ErrorLevel, "", args, skipFramesNum)
+}
+
+func Warnv(args ...interface{}) {
+	logCore(g, WarnLevel, "", args, skipFramesNum)
+}
+
+func Infov(args ...interface{}) {
+	logCore(g, InfoLevel, "", args, skipFramesNum)
+}
+
+func Debugv(args ...interface{}) {
+	logCore(g, DebugLevel, "", args, skipFramesNum)
+}
+
+func Tracev(args ...interface{}) {
+	logCore(g, TraceLevel, "", args, skipFramesNum)
+}
+
+// Logf methods
+
+func Logf(lvl Level, format string, args ...interface{}) {
+	logCore(g, lvl, format, args, skipFramesNum)
+}
+
+func Errorf(format string, args ...interface{}) {
+	logCore(g, ErrorLevel, format, args, skipFramesNum)
+}
+
+func Warnf(format string, args ...interface{}) {
+	logCore(g, WarnLevel, format, args, skipFramesNum)
+}
+
+func Infof(format string, args ...interface{}) {
+	logCore(g, InfoLevel, format, args, skipFramesNum)
+}
+
+func Debugf(format string, args ...interface{}) {
+	logCore(g, DebugLevel, format, args, skipFramesNum)
+}
+
+func Tracef(format string, args ...interface{}) {
+	logCore(g, TraceLevel, format, args, skipFramesNum)
+}
+
+func Flush() {
+	g.Flush()
+}
+
+func With(name string, val interface{}) *Logger {
+	return g.With(name, val)
+}
+
+func WithFields(fields []Field) *Logger {
+	return g.WithFields(fields)
+}
--- log/log.go
+++ log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"os"
 	"runtime"
 	"time"
 )
@@ -52,11 +51,18 @@ type Metadata struct {
 	Line   int
 }
 
-var defaultSkipFrames = 3
+const skipFramesNum = 4
 
-func newRecord(l *Logger, lvl Level, format string, args []interface{}, skipStackFrameN int) Record {
+func logCore(l *Logger, lvl Level, format string, args []interface{}, skipStackFramesNum int) {
+	if l.IsEnabled(lvl) {
+		r := newRecord(l, lvl, format, args, skipStackFramesNum)
+		l.sink.Log(&r)
+	}
+}
+
+func newRecord(l *Logger, lvl Level, format string, args []interface{}, skipStackFramesNum int) Record {
 	// Add skip arg here, and do runtime.Callers if needed to set File, and Line
-	return Record{Meta: newMetadata(l, lvl, skipStackFrameN), Format: format, Args: args}
+	return Record{Meta: newMetadata(l, lvl, skipStackFramesNum), Format: format, Args: args}
 }
 
 func newMetadata(l *Logger, lvl Level, skip int) Metadata {
@@ -74,95 +80,86 @@ func newMetadata(l *Logger, lvl Level, skip int) Metadata {
 	return m
 }
 
-func (l *Logger) Logf(lvl Level, format string, args ...interface{}) {
-	if l.IsEnabled(lvl) {
-		r := newRecord(l, lvl, format, args, defaultSkipFrames)
-		l.sink.Log(&r)
-	}
-}
+// Log methods
 
-// Note: This is duplicated here instead of using Logf
-// in order to optimize the path and prevent allocations
-// when level is not enabled.
 func (l *Logger) Log(lvl Level, message string) {
-	if l.IsEnabled(lvl) {
-		r := newRecord(l, lvl, message, nil, defaultSkipFrames)
-		l.sink.Log(&r)
-	}
+	logCore(l, lvl, message, nil, skipFramesNum)
 }
-
-func (l *Logger) Logv(lvl Level, args ...interface{}) {
-	l.Logf(lvl, "", args...)
-}
-
-func (l *Logger) IsEnabled(lvl Level) bool {
-	return l.filter(lvl)
-}
-
-// Leveled Log methods
 
 func (l *Logger) Error(message string) {
-	l.Log(ErrorLevel, message)
+	logCore(l, ErrorLevel, message, nil, skipFramesNum)
 }
 
 func (l *Logger) Warn(message string) {
-	l.Log(WarnLevel, message)
+	logCore(l, WarnLevel, message, nil, skipFramesNum)
 }
 
 func (l *Logger) Info(message string) {
-	l.Log(InfoLevel, message)
+	logCore(l, InfoLevel, message, nil, skipFramesNum)
 }
 
 func (l *Logger) Debug(message string) {
-	l.Log(DebugLevel, message)
+	logCore(l, DebugLevel, message, nil, skipFramesNum)
 }
 
 func (l *Logger) Trace(message string) {
-	l.Log(TraceLevel, message)
+	logCore(l, TraceLevel, message, nil, skipFramesNum)
 }
 
 // Logv methods
 
-func (l *Logger) Errorv(args ...interface{}) {
-	l.Logv(ErrorLevel, args...)
+func (l *Logger) Logv(lvl Level, args ...interface{}) {
+	logCore(l, lvl, "", args, skipFramesNum)
 }
 
-func (l *Logger) WarnV(args ...interface{}) {
-	l.Logv(WarnLevel, args...)
+func (l *Logger) Errorv(args ...interface{}) {
+	logCore(l, ErrorLevel, "", args, skipFramesNum)
+}
+
+func (l *Logger) Warnv(args ...interface{}) {
+	logCore(l, WarnLevel, "", args, skipFramesNum)
 }
 
 func (l *Logger) Infov(args ...interface{}) {
-	l.Logv(InfoLevel, args...)
+	logCore(l, InfoLevel, "", args, skipFramesNum)
 }
 
 func (l *Logger) Debugv(args ...interface{}) {
-	l.Logv(DebugLevel, args...)
+	logCore(l, DebugLevel, "", args, skipFramesNum)
 }
 
 func (l *Logger) Tracev(args ...interface{}) {
-	l.Logv(TraceLevel, args...)
+	logCore(l, TraceLevel, "", args, skipFramesNum)
 }
 
 // Logf methods
 
+func (l *Logger) Logf(lvl Level, format string, args ...interface{}) {
+	logCore(l, lvl, format, args, skipFramesNum)
+}
+
 func (l *Logger) Errorf(format string, args ...interface{}) {
-	l.Logf(ErrorLevel, format, args...)
+	logCore(l, ErrorLevel, format, args, skipFramesNum)
 }
 
 func (l *Logger) Warnf(format string, args ...interface{}) {
-	l.Logf(WarnLevel, format, args...)
+	logCore(l, WarnLevel, format, args, skipFramesNum)
 }
 
 func (l *Logger) Infof(format string, args ...interface{}) {
-	l.Logf(InfoLevel, format, args...)
+	logCore(l, InfoLevel, format, args, skipFramesNum)
 }
 
 func (l *Logger) Debugf(format string, args ...interface{}) {
-	l.Logf(DebugLevel, format, args...)
+	logCore(l, DebugLevel, format, args, skipFramesNum)
 }
 
 func (l *Logger) Tracef(format string, args ...interface{}) {
-	l.Logf(TraceLevel, format, args...)
+	logCore(l, TraceLevel, format, args, skipFramesNum)
+}
+
+func (l *Logger) IsEnabled(lvl Level) bool {
+	return l.filter(lvl)
 }
 
 func (l *Logger) Flush() {
@@ -181,146 +178,4 @@ func (l *Logger) WithFields(fields []Field) *Logger {
 	s = append(s, l.fields...)
 	s = append(s, fields...)
 	return &Logger{l.sink, l.filter, s, l.flags}
-}
-
-func Logf(lvl Level, format string, args ...interface{}) {
-	g.Logf(lvl, format, args...)
-}
-
-func Log(lvl Level, message string) {
-	g.Log(lvl, message)
-}
-
-func Logv(lvl Level, args ...interface{}) {
-	g.Logv(lvl, args...)
-}
-
-// Log methods
-
-func Error(message string) {
-	g.Error(message)
-}
-
-func Warn(message string) {
-	g.Warn(message)
-}
-
-func Info(message string) {
-	g.Info(message)
-}
-
-func Debug(message string) {
-	g.Debug(message)
-}
-
-func Trace(message string) {
-	g.Trace(message)
-}
-
-// Logv methods
-
-func Errorv(args ...interface{}) {
-	g.Errorv(args...)
-}
-
-func WarnV(args ...interface{}) {
-	g.WarnV(args...)
-}
-
-func Infov(args ...interface{}) {
-	g.Infov(args...)
-}
-
-func Debugv(args ...interface{}) {
-	g.Debugv(args...)
-}
-
-func Tracev(args ...interface{}) {
-	g.Tracev(args...)
-}
-
-// Logf methods
-
-func Errorf(format string, args ...interface{}) {
-	g.Errorf(format, args...)
-}
-
-func Warnf(format string, args ...interface{}) {
-	g.Warnf(format, args...)
-}
-
-func Infof(format string, args ...interface{}) {
-	g.Infof(format, args...)
-}
-
-func Debugf(format string, args ...interface{}) {
-	g.Debugf(format, args...)
-}
-
-func Tracef(format string, args ...interface{}) {
-	g.Tracef(format, args...)
-}
-
-func Flush() {
-	g.Flush()
-}
-
-func With(name string, val interface{}) *Logger {
-	return g.With(name, val)
-}
-
-func WithFields(fields []Field) *Logger {
-	return g.WithFields(fields)
-}
-
-var (
-	g *Logger
-)
-
-func init() {
-	g = &Logger{
-		sink: &StreamSink{
-			Stream:    os.Stderr,
-			Formatter: DefaultTextFormatterForHuman,
-		},
-	}
-}
-
-func SetLogger(l *Logger) {
-	g = l
-}
-
-func GetLogger() *Logger {
-	return g
-}
-
-func New(sink Sink) *Logger {
-	return &Logger{sink, AllLevelsFilter, nil, FlagTime}
-}
-
-func GetSink(logger *Logger) Sink {
-	return logger.sink
-}
-
-func GetFilter(logger *Logger) func(Level) bool {
-	return logger.filter
-}
-
-func SetFilter(logger *Logger, filter func(Level) bool) {
-	if filter == nil {
-		filter = AllLevelsFilter
-	}
-	logger.filter = filter
-}
-
-func GetFields(logger *Logger) []Field {
-	return logger.fields
-}
-
-func GetFlags(logger *Logger) loggerFlags {
-	return logger.flags
-}
-
-func SetFlags(logger *Logger, flags loggerFlags) {
-	logger.flags = flags
 }
