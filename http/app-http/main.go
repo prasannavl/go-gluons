b4897b5e3e3c82b3f31cb91feccf73fa416c89be
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-20 14:12:08 +0530

    add: logcolor, better error handling

--- http/app-http/main.go
+++ http/app-http/main.go
@@ -26,7 +26,8 @@ type EnvFlags struct {
 	Verbosity      int
 	DisplayVersion bool
 	DiagAddr       string
-	LogHumanize    bool
+	LogHumanize    int
+	LogEnableColor bool
 	Insecure       bool
 	RedirectorAddr string
 	UseSelfSigned  bool
@@ -36,16 +37,18 @@ type EnvFlags struct {
 }
 
 func initFlags(env *EnvFlags) {
+	flag.CommandLine.Init("default", flag.PanicOnError)
 	flag.BoolVar(&env.DisplayVersion, "version", false, "display the version and exit")
 	flag.CountVarP(&env.Verbosity, "verbose", "v", "verbosity level")
 	flag.StringVarP(&env.Addr, "address", "a", "localhost:8000", "the 'host:port' for the service to listen on")
 	flag.StringVar(&env.DiagAddr, "dapi-address", "localhost:9090", "the 'host:port' for diagnostics api")
 	flag.StringVar(&env.LogFile, "log", "", "the log file destination")
 	flag.BoolVar(&env.LogDisabled, "no-log", false, "disable the logger")
-	flag.BoolVarP(&env.LogHumanize, "log-humanize", "h", false, "humanize log messages")
+	flag.IntVarP(&env.LogHumanize, "log-humanize", "h", -1, "log humanize mode: -1 (auto), 0 (false), 1 (true)")
+	flag.BoolVar(&env.LogEnableColor, "log-color", true, "enable colored log messages")
 	flag.BoolVar(&env.Insecure, "insecure", false, "disable tls")
 	flag.BoolVar(&env.UseSelfSigned, "self-signed", false, "use randomly generated self signed certificate for tls")
-	flag.StringVar(&env.RedirectorAddr, "redirector", "", "a redirector address as 'host:port' to enable")
+	flag.StringVar(&env.RedirectorAddr, "redirector", "", "a http redirector address as 'host:port' to enable")
 	flag.StringArrayVar(&env.Hosts, "hosts", nil, "'host:port' items to enable hosts filter")
 	flag.StringVar(&env.WebRoot, "root", "", "web root path")
 	flag.StringVar(&env.CertCacheDir, "cert-dir", "", "the auto-tls certificate cache dir")
@@ -62,12 +65,11 @@ func initLogging(env *EnvFlags) logconfig.LogInitResult {
 	logInitResult := logconfig.LogInitResult{}
 	if !env.LogDisabled {
 		logOpts := logconfig.DefaultOptions()
-		if !env.LogHumanize {
-			logOpts.Humanize = logconfig.Humanize.False
-		}
+		logOpts.Humanize = env.LogHumanize
 		if env.LogFile != "" {
 			logOpts.LogFile = env.LogFile
 		}
+		logOpts.EnableColor = env.LogEnableColor
 		logOpts.VerbosityLevel = env.Verbosity
 		logconfig.Init(&logOpts, &logInitResult)
 	}
@@ -83,19 +85,19 @@ func printPackageHeader(versionOnly bool) {
 }
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Errorv(err)
+			log.Trace(string(debug.Stack()))
+		}
+	}()
+
 	appx.InitTerm()
 
 	env := EnvFlags{}
 	initFlags(&env)
 
 	flag.Parse()
-
-	defer func() {
-		if err := recover(); err != nil {
-			log.Errorv(err)
-			log.Error(string(debug.Stack()))
-		}
-	}()
 
 	if env.DisplayVersion {
 		printPackageHeader(true)
