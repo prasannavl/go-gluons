0d8499d6a9a191b61a99037acb37fabe4d18d7f2
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-21 16:29:42 +0530

    add: SendContentFromReader variants

--- http/handlerutils/misc.go
+++ http/handlerutils/misc.go
@@ -42,12 +42,32 @@ func SendFromReader(w http.ResponseWriter, reader io.Reader, status int) error {
 	return err
 }
 
+func SendContentFromReader(w http.ResponseWriter, reader io.Reader, contentType string, status int) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", contentType)
+	_, err := io.Copy(w, reader)
+	return err
+}
+
 func SendFromReaderHandler(reader io.Reader, status int) mchain.Handler {
 	if reader == nil {
 		return SendStatusHandler(status)
 	}
 	f := func(w http.ResponseWriter, r *http.Request) error {
 		return SendFromReader(w, reader, status)
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func SendContentFromReaderHandler(reader io.Reader, contentType string, status int) mchain.Handler {
+	if reader == nil {
+		return SendStatusHandler(status)
+	}
+	if contentType == "" {
+		return SendFromReaderHandler(reader, status)
+	}
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		return SendContentFromReader(w, reader, contentType, status)
 	}
 	return mchain.HandlerFunc(f)
 }
