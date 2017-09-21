203cab44fd1563541619c588daea1103e5666dbf
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-21 16:19:36 +0530

    add: function helper handlers

--- http/handlerutils/misc.go
+++ http/handlerutils/misc.go
@@ -51,3 +51,35 @@ func SendFromReaderHandler(reader io.Reader, status int) mchain.Handler {
 	}
 	return mchain.HandlerFunc(f)
 }
+
+func InvokeFunctionHandler(fn func()) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		fn()
+		return nil
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func FunctionResultHandler(fn func() error) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		return fn()
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func InvokeFunctionStatusHandler(fn func(), status int) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		w.WriteHeader(status)
+		fn()
+		return nil
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func FunctionResultStatusHandler(fn func() error, status int) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		w.WriteHeader(status)
+		return fn()
+	}
+	return mchain.HandlerFunc(f)
+}
