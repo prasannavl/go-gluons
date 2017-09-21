62eea8e59ad00791a5cfccd97113274987a355f9
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-09-21 16:10:17 +0530

    add: file, reader handler helpers

--- http/handlerutils/misc.go
+++ http/handlerutils/misc.go
@@ -16,18 +16,38 @@ func NopHandler() mchain.Handler {
 	return mchain.HandlerFunc(Nop)
 }
 
-func FileHandler(filePath string, status int) mchain.Handler {
+func SendFileHandler(filePath string, status int) mchain.Handler {
 	f := func(w http.ResponseWriter, r *http.Request) error {
 		f, err := os.Open(filePath)
 		if err != nil {
 			return err
 		}
+		defer f.Close()
+		return SendFromReader(w, f, status)
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func SendStatusHandler(status int) mchain.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) error {
 		w.WriteHeader(status)
-		_, err = io.Copy(w, f)
-		if err != nil {
-			return err
-		}
 		return nil
+	}
+	return mchain.HandlerFunc(f)
+}
+
+func SendFromReader(w http.ResponseWriter, reader io.Reader, status int) error {
+	w.WriteHeader(status)
+	_, err := io.Copy(w, reader)
+	return err
+}
+
+func SendFromReaderHandler(reader io.Reader, status int) mchain.Handler {
+	if reader == nil {
+		return SendStatusHandler(status)
+	}
+	f := func(w http.ResponseWriter, r *http.Request) error {
+		return SendFromReader(w, reader, status)
 	}
 	return mchain.HandlerFunc(f)
 }
